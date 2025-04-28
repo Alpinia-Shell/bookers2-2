745330ec4e86aaf83f17199feacd3b224c3bd801
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-04-28 11:06:07 +0000

    [Finish] テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,11 +19,25 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit 
+    login_user_confirmation
     @book = Book.find(params[:id])
   end
+
+  def update
+    login_user_confirmation
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book)
+    else
+    render :edit
+    end
+  end 
 
   def destroy
     Book.find(params[:id]).destroy
@@ -35,4 +49,12 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def login_user_confirmation
+    book = Book.find(params[:id])
+    unless book.user == current_user 
+    redirect_to books_path
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true, length:{minimum:2, maximum:20}
   validates :introduction, length:{maximum:50}
   has_many :books, dependent: :destroy
-  has_one_attached :image
+  has_one_attached :profile_image
 
   def get_profile_image(width, height)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
 
-    image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
